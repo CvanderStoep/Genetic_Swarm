a52e3696f175fdf3c1c80e298548b3501467b1ce
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2019-04-30 19:18:17 +0200

    initial commit

---
+++ src/Maze.java
@@ -0,0 +1,34 @@
+public class Maze {
+}
+
+
+
+// VB code
+//    Public Class Maze
+//        Private Property Opened As Boolean(,)
+//        Public Property sizeX
+//        Public Property sizeY
+//
+//        Public Sub New(ByVal sizeX As Integer, sizeY As Integer)
+//        ReDim Opened(sizeX - 1, sizeY - 1)
+    //        Me.sizeX = sizeX
+    //        Me.sizeY = sizeY
+    //        For i As Integer = 0 To (sizeX - 1)
+        //        For j As Integer = 0 To (sizeY - 1)
+        //          Me.Opened(i, j) = True
+        //        Next
+    //        Next
+//        End Sub
+//
+//        Public Function IsOpen(i As Integer, j As Integer) As Boolean
+//        If (i < 0 Or j < 0) Then Return True
+//        If (i > sizeX - 1 Or j > sizeX - 1) Then Return False
+//        Return Me.Opened(i, j)
+//        End Function
+//
+//        Public Sub SetOpenStatus(i As Integer, j As Integer, open As Boolean)
+//          Opened(i, j) = open
+//        End Sub
+//
+//      End Class
+
