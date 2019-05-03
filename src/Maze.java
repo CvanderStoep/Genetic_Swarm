acc0832e91e301209000de4bc21da2e7fe8c3c3f
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2019-05-03 17:41:37 +0200

    variable in routine renamed

--- src/Maze.java
+++ src/Maze.java
@@ -3,16 +3,15 @@ public class Maze {
     public int sizeX;
     public int sizeY;
 
-    // Genereert een speelveld met go/no-go area's
 
-    public Maze(int sizeX, int sizeY) {
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
+    public Maze(int nX, int nY) {
+        this.sizeX = nX;
+        this.sizeY = nY;
 
-        field = new boolean[sizeX][sizeY];
+        field = new boolean[nX][nY];
 
-        for (int i = 0; i < sizeX; i++) {
-            for (int j = 0; j < sizeY; j++) {
+        for (int i = 0; i < nX; i++) {
+            for (int j = 0; j < nY; j++) {
                 field[i][j] = true;
             }
         }
@@ -37,34 +36,3 @@ public class Maze {
         }
     }
 }
-
-
-// VB code
-//    Public Class Maze
-//        Private Property Opened As Boolean(,)
-//        Public Property sizeX
-//        Public Property sizeY
-//
-//        Public Sub New(ByVal sizeX As Integer, sizeY As Integer)
-//        ReDim Opened(sizeX - 1, sizeY - 1)
-//        Me.sizeX = sizeX
-//        Me.sizeY = sizeY
-//        For i As Integer = 0 To (sizeX - 1)
-//        For j As Integer = 0 To (sizeY - 1)
-//          Me.Opened(i, j) = True
-//        Next
-//        Next
-//        End Sub
-//
-//        Public Function IsOpen(i As Integer, j As Integer) As Boolean
-//        If (i < 0 Or j < 0) Then Return True
-//        If (i > sizeX - 1 Or j > sizeX - 1) Then Return False
-//        Return Me.Opened(i, j)
-//        End Function
-//
-//        Public Sub SetOpenStatus(i As Integer, j As Integer, open As Boolean)
-//          Opened(i, j) = open
-//        End Sub
-//
-//      End Class
-
